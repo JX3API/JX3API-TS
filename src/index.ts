0a556b62d8bc15f6a855d1852a3e50abaca40b3c
fine <fine_951111@163.com>
2025-01-14 22:41:37 +0800

    docs:添加注释

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,8 @@
+/**
+ * @module JX3API
+ * @description JX3API 是一个用于访问剑网3游戏相关数据的 API 客户端
+ */
+
 import { ServerService } from './services/server';
 import { NewsService } from './services/news';
 import { RoleService } from './services/role';
@@ -9,18 +14,39 @@ import { ToolsService } from './services/tools';
 import { StatisticsService } from './services/statistics';
 import { WebSocketService } from './services/websocket';
 
+/**
+ * JX3API 客户端类
+ * @class JX3API
+ */
 export class JX3API {
+  /** 服务器相关接口服务 */
   public readonly server: ServerService;
+  /** 新闻资讯相关接口服务 */
   public readonly news: NewsService;
+  /** 角色相关接口服务 */
   public readonly role: RoleService;
+  /** 战斗相关接口服务 */
   public readonly battle: BattleService;
+  /** 社交相关接口服务 */
   public readonly social: SocialService;
+  /** 经济相关接口服务 */
   public readonly economy: EconomyService;
+  /** 娱乐相关接口服务 */
   public readonly entertainment: EntertainmentService;
+  /** 工具相关接口服务 */
   public readonly tools: ToolsService;
+  /** 统计相关接口服务 */
   public readonly statistics: StatisticsService;
+  /** WebSocket 相关接口服务 */
   public readonly ws: WebSocketService;
 
+  /**
+   * 创建一个新的 JX3API 实例
+   * @param {Object} options - 配置选项
+   * @param {string} [options.token] - API 访问令牌
+   * @param {string} [options.ticket] - 用户票据
+   * @param {string} [options.baseURL] - API 基础 URL
+   */
   constructor(options: {
     token?: string;
     ticket?: string;
