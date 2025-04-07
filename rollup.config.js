3d05cd5e1e9a0bcab03774155d2c9846d9ec6728
fine <fine_951111@163.com>
2025-04-07 19:19:56 +0800

    feat:2.0.0

--- rollup.config.js
+++ rollup.config.js
@@ -2,9 +2,8 @@ import resolve from '@rollup/plugin-node-resolve';
 import typescript from '@rollup/plugin-typescript';
 import commonjs from '@rollup/plugin-commonjs';
 import del from 'rollup-plugin-delete';
-import babel from '@rollup/plugin-babel';
-import { DEFAULT_EXTENSIONS } from '@babel/core';
 import json from '@rollup/plugin-json';
+import terser from '@rollup/plugin-terser';
 export default {
   input: './src',
   output: [
@@ -14,6 +13,7 @@ export default {
       entryFileNames: '[name].cjs',
       preserveModules: true, // 保留模块结构
       sourcemap: false, // 是否输出sourcemap
+      plugins: [terser()],
       exports: 'named', // 指定导出模式（自动、默认、命名、无）
       preserveModulesRoot: 'src', // 将保留的模块放在根级别的此路径下
     },
@@ -23,24 +23,18 @@ export default {
       entryFileNames: '[name].mjs',
       preserveModules: true, // 保留模块结构
       sourcemap: false, // 是否输出sourcemap
+      plugins: [terser()],
       name: 'index',
       exports: 'named', // 指定导出模式（自动、默认、命名、无）
       preserveModulesRoot: 'src', // 将保留的模块放在根级别的此路径下
     },
   ],
+  external: [/node_modules/],
   plugins: [
     json(),
     del({ targets: ['lib'] }),
     resolve({ extensions: ['.js', '.ts'] }),
     commonjs(),
     typescript({ module: 'ESNext', sourceMap: false }),
-    babel({
-      // 编译库使用 runtime
-      babelHelpers: 'runtime',
-      // 只转换源代码，不转换外部依赖
-      exclude: 'node_modules/**',
-      // babel 默认不支持 ts 需要手动添加
-      extensions: [...DEFAULT_EXTENSIONS, '.ts'],
-    }),
   ],
 };
